5d2d111d05d53db9c04bd4b6dea98a469c0d7d0e
syukrontanzilah <syukrontanzilah@gmail.com>
2024-11-11 13:37:29 +0700

    styling karya anggota

--- src/app/(dashboard)/karyaguru/CardKaryaAnggota.tsx
+++ src/app/(dashboard)/karyaguru/CardKaryaAnggota.tsx
@@ -4,16 +4,16 @@ const CardKaryaAnggota = () => {
   return (
     <div className="w-[calc(33.333%-16px)] rounded-2xl border border-custom-aqua bg-white mb-0" >
         <div className='flex justify-between pt-4'>
-            <div className='flex px-4'>
-                <div className='h-[36px] w-[36px] rounded-md bg-[#d9d9d9]'>
+            <div className='flex pl-4 pr-0'>
+                <div className='h-[36px] w-[36px] rounded-md bg-[#d9d9d9] flex'>
                     {/* <img src='' alt='img'/> */}
                 </div>
                 <div className='ml-2'>
-                    <div className='text-sm font-semibold'>Mohammad Alfath, MM, S.Kom</div>
+                    <div className='text-sm font-semibold'>Mohammad Alfath, MM, S.Kom </div>
                     <div className='text-[10px] text-[#919191]'><span>NPA 20323222</span><span className='text-[#FF5A00]'> | Banyumas</span></div>
                 </div>
             </div>
-            <div className='text-white bg-[#ff5a00] text-xs flex items-center justify-center rounded-bl-full rounded-tl-full px-3'>
+            <div className='text-white bg-[#ff5a00] text-xs flex items-center justify-center rounded-bl-full rounded-tl-full px-2 h-[32px] !min-w-[100px]'>
                 10 Karya
             </div>
         </div>
