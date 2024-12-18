6a6bb828001bd472e609bed411ac98d44c6a394f
Rizwan Gustama <rizonegustama@gmail.com>
2024-12-18 10:37:14 +0700

    update cartstatistic

--- src/app/(dashboard)/statistik/component/ChartStatistic.tsx
+++ src/app/(dashboard)/statistik/component/ChartStatistic.tsx
@@ -8,7 +8,11 @@ const HighchartsReact = dynamic(() => import('highcharts-react-official'), {
   ssr: false
 });
 
-const ChartStatistic: React.FC = () => {
+interface ChartProps {
+  categoryChart: string
+}
+
+const ChartStatistic: React.FC<ChartProps> = ({ categoryChart}) => {
   const options: Highcharts.Options = {
     title: {
       text: '',
@@ -17,7 +21,122 @@ const ChartStatistic: React.FC = () => {
       type: 'column',
     },
     xAxis: {
-      categories: ['USA', 'China', 'Brazil', 'EU', 'Argentina', 'India'], // Kategori pada sumbu X
+      categories: ['20 - 30 th', '31 - 40 th', '41 - 50 th', '51 - 60 th', '61 th ke atas'], // Kategori pada sumbu X
+      crosshair: true,
+      accessibility: {
+        description: 'Countries', // Deskripsi untuk aksesibilitas
+      },
+      gridLineWidth: 0,
+    },
+    yAxis: {
+      min: 0, // Nilai minimal pada sumbu Y
+      title: {
+        text: '', // Judul sumbu Y
+      },
+      gridLineWidth: 0,
+      labels: {
+        enabled: false,
+      }
+    },
+    plotOptions: {
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+        borderRadius: 100,
+        maxPointWidth: 34,
+      }
+    },
+    series: [
+      {
+        type: 'column', // Menambahkan tipe grafik untuk seri pertama
+        name: 'Laki-laki', // Nama seri pertama
+        data: [17400, 14400, 13000, 8000, 5000], // Data untuk seri 'Corn'
+      },
+      {
+        type: 'column', // Menambahkan tipe grafik untuk seri kedua
+        name: 'Perempuan', // Nama seri kedua
+        data: [16400, 13400, 11000, 14000, 4000], // Data untuk seri 'Wheat'
+      },
+    ],
+    legend: {
+      enabled: false,
+    },
+    tooltip: {
+      shared: true,
+      valueSuffix: ' Orang',
+    },
+  };
+
+  const qualification: Highcharts.Options = {
+    title: {
+      text: '',
+    },
+    chart: {
+      type: 'column',
+    },
+    xAxis: {
+      categories: ['SMA/SMK', 'D3', 'S1', 'S2', 'S3'], // Kategori pada sumbu X
+      crosshair: true,
+      accessibility: {
+        description: 'Countries', // Deskripsi untuk aksesibilitas
+      },
+      gridLineWidth: 0,
+    },
+    yAxis: {
+      min: 0, // Nilai minimal pada sumbu Y
+      title: {
+        text: '', // Judul sumbu Y
+      },
+      labels: {
+        enabled: false,
+      },
+      gridLineWidth: 0,
+    },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true
+        },
+        groupPadding: 0.1
+      },
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+        maxPointWidth: 34,
+        borderRadius: 100,
+      }
+    },
+    series: [
+      {
+        type: 'column', // Menambahkan tipe grafik untuk seri pertama
+        name: 'Kualifikasi Pendidikan', // Nama seri pertama
+        data: [
+          { y: 200, color: '#2CAFFE' }, // Warna merah
+          { y: 500, color: '#544FC5' }, // Warna hijau
+          { y: 1000, color: '#20C997' }, // Warna biru
+          { y: 400, color: '#FD7E14' }, // Warna kuning
+          { y: 700, color: '#007BFF' }, // Warna ungu
+        ],
+      },
+    ],
+    tooltip: {
+      shared: true,
+      valueSuffix: ' units',
+    },
+  };
+
+  const teachingPlace: Highcharts.Options = {
+    title: {
+      text: '',
+    },
+    chart: {
+      type: 'column',
+    },
+    xAxis: {
+      categories: ['Sudah Bersertifikat', 'Belum Bersertifkat'], // Kategori pada sumbu X
       crosshair: true,
       accessibility: {
         description: 'Countries', // Deskripsi untuk aksesibilitas
@@ -30,21 +149,33 @@ const ChartStatistic: React.FC = () => {
       },
     },
     plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true
+        },
+        groupPadding: 0.1
+      },
       column: {
         pointPadding: 0.2,
-        borderWidth: 0
+        borderWidth: 0,
+        maxPointWidth: 34,
+        borderRadius: 100,
       }
     },
     series: [
       {
         type: 'column', // Menambahkan tipe grafik untuk seri pertama
-        name: 'Corn', // Nama seri pertama
-        data: [387749, 280000, 129000, 64300, 54000, 34300], // Data untuk seri 'Corn'
-      },
-      {
-        type: 'column', // Menambahkan tipe grafik untuk seri kedua
-        name: 'Wheat', // Nama seri kedua
-        data: [45321, 140000, 10000, 140500, 19500, 113500], // Data untuk seri 'Wheat'
+        name: 'Kualifikasi Pendidikan', // Nama seri pertama
+        data: [
+          { y: 200, color: '#2CAFFE' }, // Warna merah
+          { y: 500, color: '#544FC5' }, // Warna hijau
+          { y: 1000, color: '#20C997' }, // Warna biru
+          { y: 400, color: '#FD7E14' }, // Warna kuning
+          { y: 700, color: '#007BFF' }, // Warna ungu
+          { y: 700, color: '#D568FB' },
+          { y: 700, color: '#2EE0CA' },
+          { y: 700, color: '#DB2424' },
+        ],
       },
     ],
     tooltip: {
@@ -53,17 +184,131 @@ const ChartStatistic: React.FC = () => {
     },
   };
 
+  const teacherCertificate: Highcharts.Options = {
+    title: {
+      text: '',
+    },
+    chart: {
+      type: 'column',
+    },
+    xAxis: {
+      categories: ['Sudah Bersertifikat', 'Belum Bersertifikat'], // Kategori pada sumbu X
+      crosshair: true,
+      accessibility: {
+        description: 'Countries', // Deskripsi untuk aksesibilitas
+      },
+    },
+    yAxis: {
+      min: 0, // Nilai minimal pada sumbu Y
+      title: {
+        text: 'Amount', // Judul sumbu Y
+      },
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true
+        },
+        groupPadding: 0.1
+      },
+      column: {
+        pointPadding: 0.2,
+        borderWidth: 0,
+        maxPointWidth: 34,
+        borderRadius: 100,
+      }
+    },
+    series: [
+      {
+        type: 'column', // Menambahkan tipe grafik untuk seri pertama
+        name: 'Kualifikasi Pendidikan', // Nama seri pertama
+        data: [
+          { y: 200, color: '#2CAFFE' }, // Warna merah
+          { y: 500, color: '#544FC5' }, // Warna hijau
+        ],
+      },
+    ],
+    legend: {
+      enabled: false,
+    },
+    tooltip: {
+      shared: true,
+      valueSuffix: ' Orang',
+    },
+  }
+
   return (
-    <div className={'p-10 bg-white rounded-2xl'}>
-      <div className={'flex justify-between mb-[34px]'}>
-        <h3>Jenis Kelamin & Umur</h3>
-        <select>
-          <option>Januari - Desember 2024</option>
-        </select>
-      </div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
-    </div>
-  );
+    <>
+      { categoryChart  == 'C01' && (
+          <div className={'p-10 bg-white rounded-2xl'}>
+            <div className={'flex justify-between mb-[34px]'}>
+              <h3>Jenis Kelamin & Umur</h3>
+              <select>
+                <option>Januari - Desember 2024</option>
+              </select>
+            </div>
+            <HighchartsReact highcharts={Highcharts} options={options} />
+          </div>
+        )
+      }
+
+      {
+        categoryChart  == 'C02' && (
+          <div className={'p-10 bg-white rounded-2xl'}>
+            <div className={'flex justify-between mb-[34px]'}>
+              <h3>Kualifikasi Pendidikan</h3>
+              <select>
+                <option>Januari - Desember 2024</option>
+              </select>
+            </div>
+            <HighchartsReact highcharts={Highcharts} options={qualification} />
+          </div>
+        )
+      }
+
+      {
+        categoryChart  == 'C03' && (
+          <div className={'p-10 bg-white rounded-2xl'}>
+            <div className={'flex justify-between mb-[34px]'}>
+              <h3>Tempat Mengajar</h3>
+              <select>
+                <option>Januari - Desember 2024</option>
+              </select>
+            </div>
+            <HighchartsReact highcharts={Highcharts} options={teachingPlace} />
+          </div>
+        )
+      }
+
+      {
+        categoryChart  == 'C04' && (
+          <div className={'p-10 bg-white rounded-2xl'}>
+            <div className={'flex justify-between mb-[34px]'}>
+              <h3>Tempat Mengajar</h3>
+              <select>
+                <option>Januari - Desember 2024</option>
+              </select>
+            </div>
+            <HighchartsReact highcharts={Highcharts} options={teachingPlace} />
+          </div>
+        )
+      }
+
+      {
+        categoryChart  == 'C05' && (
+          <div className={'p-10 bg-white rounded-2xl'}>
+            <div className={'flex justify-between mb-[34px]'}>
+              <h3>Sertifikat Pendidik</h3>
+              <select>
+                <option>Januari - Desember 2024</option>
+              </select>
+            </div>
+            <HighchartsReact highcharts={Highcharts} options={teacherCertificate} />
+          </div>
+        )
+      }
+    </>
+  )
 };
 
 export default ChartStatistic;
--- src/app/(dashboard)/statistik/page.tsx
+++ src/app/(dashboard)/statistik/page.tsx
@@ -1,24 +1,64 @@
-import React, { Fragment } from "react";
+"use client";
+
+import React, { Fragment, useState } from "react";
 import ChartStatistic from "@/app/(dashboard)/statistik/component/ChartStatistic";
+import clsx from "clsx";
+
+interface ICategory {
+  label: string,
+  value: string,
+}
 
 const StatistikPage = () => {
+  const categoryStatistic: ICategory[] = [
+    {
+      label: "Jenis Kelamin & Umur",
+      value: 'C01'
+    },
+    {
+      label: "Kualifikasi Pendidikan",
+      value: 'C02'
+    },
+    {
+      label: "Tempat Mengajar",
+      value: 'C03'
+    },
+    {
+      label: "Provinsi",
+      value: 'C04'
+    },
+    {
+      label: "Sertifikat Pendidik",
+      value: 'C05'
+    }
+  ]
+
+  const [selectCategoryStatistic, setSelectCategoryStatistic] = useState<string>('C01');
+
+  const handleClickCategory = (category: ICategory) => {
+    setSelectCategoryStatistic(category.value)
+  }
   return (
     <Fragment>
       <div className={'flex justify-between mb-8'}>
         <h2 className={'text-primary font-semibold'}>Statistik Data Anggota | 0-100K</h2>
         <div className={'flex gap-3.5'}>
-          <button className={'text-xs bg-primary py-2.5 px-2.5 text-white rounded-[10px]'}>
-            Jenis Kelamin & Umur
-          </button>
-          <button className={'text-xs bg-primary py-2.5 px-2.5 text-white rounded-[10px]'}>
-            Jenis Kelamin & Umur
-          </button>
-          <button className={'text-xs bg-primary py-2.5 px-2.5 text-white rounded-[10px]'}>
-            Jenis Kelamin & Umur
-          </button>
+          { categoryStatistic.map((category: ICategory, index: number) => (
+            <button
+              onClick={() => handleClickCategory(category)}
+              key={index}
+              className={clsx(
+                'text-xs  py-2.5 px-2.5 rounded-[10px]',
+                { 'bg-primary text-white border border-primary': selectCategoryStatistic === category.value },
+                { 'bg-white text-primary border border-primary': selectCategoryStatistic !== category.value }
+              )}
+            >
+              {category.label}
+            </button>
+          ))}
         </div>
       </div>
-      <ChartStatistic />
+      <ChartStatistic categoryChart={selectCategoryStatistic}/>
     </Fragment>
   )
 }
