5f2bfcbd0812f70cdd374e9344838541f8a5aa97
Amirul Muminin <almin.cesc@gmail.com>
2025-07-07 10:26:18 +0700

    fix: table value styling

--- src/app/(dashboard)/cetak-kta/components/table/CellComponents.tsx
+++ src/app/(dashboard)/cetak-kta/components/table/CellComponents.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CetakKta } from "../../../../../../public/icon/sidebarIcon/CetakKta";
 
 interface ActionCellProps {
   onCR80Click?: () => void;
@@ -12,14 +13,7 @@ export const ActionCell: React.FC<ActionCellProps> = ({
   return (
     <div className="flex flex-1 items-center justify-start gap-2">
       <div className="flex flex-1 items-center justify-center gap-2 px-2.5 py-1">
-        <div className="relative h-[18px] w-[18px]">
-          <div className="absolute left-[5.44px] top-[1.50px] h-[3.75px] w-[7.12px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17191c]" />
-          <div className="absolute left-[6px] top-[11.25px] h-[5.25px] w-1.5 outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17191c]" />
-          <div className="absolute left-[2.25px] top-[5.25px] h-[8.25px] w-[13.50px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17191c]" />
-          <div className="absolute left-[5.25px] top-[11.25px] h-0 w-[7.50px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17191c]" />
-          <div className="absolute left-[5.25px] top-[8.25px] h-0 w-[2.25px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17191c]" />
-          <div className="absolute left-0 top-0 h-[18px] w-[18px] opacity-0" />
-        </div>
+        <CetakKta customColor="#17191c" />
         <button
           onClick={onCR80Click}
           className="justify-start text-xs font-normal text-[#17191c]"
@@ -27,16 +21,10 @@ export const ActionCell: React.FC<ActionCellProps> = ({
           CR80
         </button>
       </div>
-      <div className="h-0 w-5 origin-top-left rotate-90 outline outline-1 outline-offset-[-0.50px] outline-[#ff0000]" />
+      {/* Divider vertikal */}
+      <div className="mx-1 h-6 w-px bg-[#ff0000]" />
       <div className="flex flex-1 items-center justify-center gap-2 px-2.5 py-1">
-        <div className="relative h-[18px] w-[18px]">
-          <div className="absolute left-[5.44px] top-[1.50px] h-[3.75px] w-[7.12px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17a3b8]" />
-          <div className="absolute left-[6px] top-[11.25px] h-[5.25px] w-1.5 outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17a3b8]" />
-          <div className="absolute left-[2.25px] top-[5.25px] h-[8.25px] w-[13.50px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17a3b8]" />
-          <div className="absolute left-[5.25px] top-[11.25px] h-0 w-[7.50px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17a3b8]" />
-          <div className="absolute left-[5.25px] top-[8.25px] h-0 w-[2.25px] outline outline-[1.50px] outline-offset-[-0.75px] outline-[#17a3b8]" />
-          <div className="absolute left-0 top-0 h-[18px] w-[18px] opacity-0" />
-        </div>
+        <CetakKta customColor="#17a3b8" />
         <button
           onClick={onCR79Click}
           className="justify-start text-xs font-normal text-[#17a3b8]"
@@ -55,10 +43,15 @@ interface PhotoCellProps {
 
 export const PhotoCell: React.FC<PhotoCellProps> = ({ src, alt = "Foto" }) => {
   if (src) {
+    // Gambar berada di atas styled div (z-index lebih tinggi)
     return (
-      <div className="flex items-center justify-center">
+      <div className="relative">
         <div className="h-6 w-6 rounded border border-[#17a3b8] bg-white" />
-        <img className="h-[18px] w-[18px] rounded-sm" src={src} alt={alt} />
+        <img
+          className="absolute left-[2px] top-[2px] z-10 h-[20px] w-[20px] rounded-[2px]"
+          src={src}
+          alt={alt}
+        />
       </div>
     );
   }
@@ -94,12 +87,30 @@ export const StatusCell: React.FC<StatusCellProps> = ({ status }) => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="relative h-[18px] w-[18px] overflow-hidden">
-        <div
-          className={`absolute left-[1.50px] top-[1.50px] h-[15px] w-[15px] ${
-            isActive ? "bg-[#17a3b8]" : "bg-[#ff0000]"
-          }`}
-        />
+      <div className={`flex h-[18px] w-[18px] items-center justify-center rounded-full ${
+        isActive ? "bg-[#17a3b8]" : "bg-[#ff0000]"
+      }`}>
+        {isActive ? (
+          <svg width="10" height="8" viewBox="0 0 10 8" fill="none">
+            <path
+              d="M1 4L3.5 6.5L9 1"
+              stroke="white"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        ) : (
+          <svg width="8" height="8" viewBox="0 0 8 8" fill="none">
+            <path
+              d="M1 1L7 7M7 1L1 7"
+              stroke="white"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        )}
       </div>
     </div>
   );
